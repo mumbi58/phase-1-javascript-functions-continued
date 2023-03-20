74c1a31e23fe2a719580db3f9726e317852c5d44
mumbi58 <126749794+mumbi58@users.noreply.github.com>
2023-03-20 14:55:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(two="roller-skate"){
+    return `This Saturday, I want to ${two}!`;
+  }
+  saturdayFun("bathe my dog");
+  function mondayWork(one="go to the office"){
+    return `This Monday, I will ${one}.`;}
+  function wrapAdjective(adjective="*"){
+  const innerF= function(three="special"){
+    return `You are ${adjective}${three}${adjective}!`;
+  }
+  return innerF;
+  }
